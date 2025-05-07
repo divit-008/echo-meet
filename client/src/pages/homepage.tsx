263f3d69fea42efa1016e7be027fa900bbde4c9b
Divit <divit3008@gmail.com>
2025-05-06 17:57:36 -0700

    Built the auth

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -1,6 +1,21 @@
+import { supabase } from "../supabaseClient"
+import { useNavigate } from "react-router-dom"
 const homepage = () => {
+    const navigate = useNavigate()
+    const handleSignIn = async () =>{
+        const {error} = await supabase.auth.signInWithOAuth({
+          provider: 'google',
+          options: {
+            redirectTo: 'http://localhost:5173/dashboard',
+          }
+        })
+        if (error) console.error("Error Logging in :", error.message)
+    }
   return (
-    <div>homepage</div>
+    <div>
+      <h1>A lightweight, real-time video meeting app with user accounts, room hosting/joining via code or link, and a responsive UI.</h1>
+      <button onClick={handleSignIn}>Sign In with google</button>
+    </div>
   )
 }
 

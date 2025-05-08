459c2aa8388c077edcb368cd63e6180e4a3b2e22
Divit <divit3008@gmail.com>
2025-05-07 21:59:14 -0700

    minor fixes

--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -6,7 +6,7 @@ const Homepage = () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google", // Specify Google as the OAuth provider
       options: {
-        redirectTo: "https://echo-meet-eight.vercel.app/dashboard", // Redirect after successful sign-in
+        redirectTo: `${window.location.origin}/dashboard`, // Redirect after successful sign-in
       },
     });
 

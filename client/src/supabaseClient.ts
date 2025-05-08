8b8eadc5f40e1c9d442b4cacb3225ec83810de0f
Divit <divit3008@gmail.com>
2025-05-07 21:40:39 -0700

    Fixed security issues and folder structure.

--- client/src/supabaseClient.ts
+++ client/src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qqrugjnzdebuepjjsdop.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxcnVnam56ZGVidWVwampzZG9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MDk0NTQsImV4cCI6MjA2MjA4NTQ1NH0.8A3_OAVhrqAbw1p4rT1SOK8KgKz4QzxLCLetZoY9rxM'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
